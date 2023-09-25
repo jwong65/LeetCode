652a7773470c4c608cd1cf0cfda04fa517d96d6d
jwong65 <jason.wong6507@gmail.com>
2023-09-25 16:34:07 -0400

    Instead of finding lowest possible price check if price is more expensive in the next day

--- Array Questions/2.js
+++ Array Questions/2.js
@@ -4,8 +4,20 @@
 // For example Input: prices = [7,1,5,3,6,4] in the question the lowest possible price is 1 on the 2nd day.
 // Input: prices = [1,2,3,4,5] And this would be 1 on the first day.
 
+prices = [7,1,5,3,6,4]
+
 var maxProfit = function(prices) {
-    
+    // This is to iterate through each section
+    for (let i=0; i<prices.length; i++){
+        var profit;
+        if (prices[i]>= prices[i+1]){
+            // This is to check if the next day is too cheaper than the current day
+            continue
+        }
+        else{
+        }
+    }
+    console.log(prices)
 };
 
 // This is to run the file
