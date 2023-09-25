5d7c1a176784c3b9fa0331c20e2ed4aba6924227
jwong65 <jason.wong6507@gmail.com>
2023-09-25 15:53:42 -0400

    Added test to run the file

--- Array Questions/1.js
+++ Array Questions/1.js
@@ -3,5 +3,11 @@
 // First the integer array is nums and then we need a new array that will hold the correct sorted array
 let nums =  [1,1,2]
 var removeDuplicates = function(nums) {
-    
+    for (let i=0; i<nums.length; i++){
+
+    }
+    console.log(nums)
 };
+
+// This is to run the file
+k = removeDuplicates(nums);
