59beb40bd5ef0c1b4241a498f0b6e06a1920347d
jwong65 <jason.wong6507@gmail.com>
2023-09-25 15:42:30 -0400

    Started the first question and commented out thought process for the array folder

---
+++ Array Questions/1.js
@@ -0,0 +1,7 @@
+// Remove Duplicates from Sorted Array
+
+// First the integer array is nums and then we need a new array that will hold the correct sorted array
+let nums =  [1,1,2]
+var removeDuplicates = function(nums) {
+    
+};
