ed246240d9e9fcc9ca026633cfa1601d9d81229f
jwong65 <jason.wong6507@gmail.com>
2023-09-25 16:25:49 -0400

    Added second problem

---
+++ Array Questions/2.js
@@ -0,0 +1,12 @@
+//  Best Time to Buy and Sell Stock II
+
+// First part of the solution is through the array you need to find the lowest integer.
+// For example Input: prices = [7,1,5,3,6,4] in the question the lowest possible price is 1 on the 2nd day.
+// Input: prices = [1,2,3,4,5] And this would be 1 on the first day.
+
+var maxProfit = function(prices) {
+    
+};
+
+// This is to run the file
+k= maxProfit(prices)
