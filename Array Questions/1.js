678bd7db505a3e916bd25c77e0d5b73307fa09a9
jwong65 <jason.wong6507@gmail.com>
2023-09-25 16:19:39 -0400

    Added the first solution and comments

--- Array Questions/1.js
+++ Array Questions/1.js
@@ -3,10 +3,15 @@
 // First the integer array is nums and then we need a new array that will hold the correct sorted array
 let nums =  [1,1,2]
 var removeDuplicates = function(nums) {
+    newarray = new Array
     for (let i=0; i<nums.length; i++){
-
+        // I use this if statement to check if the next number in the array is the same, if it is not then I add it to the new array
+        if(nums[i]!=nums[i+1]){
+            newarray.push(nums[i])
+        }
     }
     console.log(nums)
+    console.log(newarray)
 };
 
 // This is to run the file
