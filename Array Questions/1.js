63d1d1fe880be67681068f957e096de614b98f37
jwong65 <jason.wong6507@gmail.com>
2023-09-25 16:22:17 -0400

    Added solution used on the website instead of compiler

--- Array Questions/1.js
+++ Array Questions/1.js
@@ -16,3 +16,19 @@ var removeDuplicates = function(nums) {
 
 // This is to run the file
 k = removeDuplicates(nums);
+
+// On the website solution submitted
+var removeDuplicates = function(nums) {
+    let i=0
+    for(let u=1; u<nums.length; u++){
+        if (nums[u]!=nums[i]){
+            nums[++i] = nums[u]
+            // newarray.push(nums[i])
+        }
+    }
+    return i+1;
+};
+
+// Using a for loop but instead of making a newarray to submit for the site I needed to use a return statement
+// the let i=0 is similar to making a new array
+// The for loop needs to have a let u=1 to check the number ahead similar to how in the first solution I used i+1.
