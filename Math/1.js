04cde6c143bba16db74cd904df02170af324e091
jwong65 <jason.wong6507@gmail.com>
2023-09-29 01:53:03 -0400

    Changed the operators to be strict equality

--- Math/1.js
+++ Math/1.js
@@ -10,13 +10,13 @@ var fizzBuzz = function(n) {
     const string1=[]
 
     for (let i=1; i<=n; i++){
-    if((i%3==0) && (i%5==0)){
+    if((i%3===0) && (i%5===0)){
         string1.push("FizzBuzz")
     }
-    else if (i%3==0){
+    else if (i%3===0){
         string1.push("Fizz")
     }
-    else if(i%5==0){
+    else if(i%5===0){
         string1.push("Buzz")
     }
     else{
