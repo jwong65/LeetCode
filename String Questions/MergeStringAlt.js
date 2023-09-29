2bfe1ca37e21aeda6c2c5f09e1002f55c7d2a604
jwong65 <jason.wong6507@gmail.com>
2023-09-29 01:30:49 -0400

    Added the question in the commented section

--- String Questions/MergeStringAlt.js
+++ String Questions/MergeStringAlt.js
@@ -3,6 +3,12 @@
  * @param {string} word2
  * @return {string}
  */
+
+// You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1. If a string is longer than the other, append the additional letters onto the end of the merged string.
+
+// Return the merged string.
+
+ 
 var mergeAlternately = function(word1, word2) {
     word1split = word1.split("")
     word2split = word2.split("")
