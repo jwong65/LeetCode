8ce4815c8cfb26bc3adb76954d8530f5c3cfef42
jwong65 <jason.wong6507@gmail.com>
2023-09-29 01:45:36 -0400

    Added the Math directory and added the first question and how I solved it

---
+++ Math/1.js
@@ -0,0 +1,27 @@
+// Fizz Buzz
+// Given an integer n, return a string array answer (1-indexed) where:
+
+// answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+// answer[i] == "Fizz" if i is divisible by 3.
+// answer[i] == "Buzz" if i is divisible by 5.
+// answer[i] == i (as a string) if none of the above conditions are true.
+n =5
+var fizzBuzz = function(n) {
+    const string1=[]
+
+    for (let i=1; i<=n; i++){
+    if((n%3==0) && (n%5==0)){
+        string1.push("FizzBuzz")
+    }
+    else if (n%3==0){
+        string1.push("Fizz")
+    }
+    else if(n%5){
+        string1.push("Buzz")
+    }
+    else{
+        string1.push(i)
+    }
+}
+    return string1
+};
